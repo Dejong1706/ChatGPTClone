2a3b2117b981f868c325782bac1a6564fb0b8aee
JanBe <ppk1998@naver.com>
2023-11-08 19:52:34 +0900

    first commit

---
+++ components/ChatSidebar/ChatSidebar.js
@@ -0,0 +1,59 @@
+import {
+  faMessage,
+  faPlus,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const ChatSidebar = ({ chatId }) => {
+  const [chatList, setChatList] = useState([]);
+  useEffect(() => {
+    const loadChatList = async () => {
+      const response = await fetch(`/api/chat/getChatList`, {
+        method: "POST",
+      });
+      const json = await response.json();
+      setChatList(json);
+    };
+    loadChatList();
+  }, [chatId]);
+  return (
+    <div className="overflex-hidden flex flex-col bg-gray-900 text-white">
+      <Link
+        href="/chat"
+        className="side-menu-item bg-emerald-500 hover:bg-emerald-600"
+      >
+        <FontAwesomeIcon icon={faPlus} />
+        New Chat
+      </Link>
+      <div className="flex-1 overflow-auto bg-gray-950">
+        {chatList.map((chat) => (
+          <Link
+            key={chat._id}
+            href={`/chat/${chat._id}`}
+            className={`side-menu-item ${
+              chatId === chat._id ? "bg-gray-700 hover:bg-gray-700" : ""
+            }`}
+          >
+            <FontAwesomeIcon icon={faMessage} className="text-white/50" />
+            <span
+              title={chat.title}
+              className="text-elipsis overflow-hidden whitespace-nowrap"
+            >
+              {chat.title}
+            </span>
+          </Link>
+        ))}
+      </div>
+      <Link href="/api/auth/logout" className="side-menu-item">
+        <FontAwesomeIcon icon={faRightFromBracket} />
+        logout
+      </Link>
+    </div>
+  );
+};
+
+export default ChatSidebar;
